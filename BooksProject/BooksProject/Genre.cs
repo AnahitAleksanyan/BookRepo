7087736a8f60376ed635ca1f552029bfdb763221
Anahit Aleksanyan <anahit_aleksanyan@epam.com>
2023-03-04 12:33:20 +0400

    created enum Genre

---
+++ BooksProject/BooksProject/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksProject
+{
+    public enum Genre
+    {
+        Thriller,
+        Horror,
+        Romance,
+        Western,
+        Drama
+    }
+}
