a368e82757ec3a9e811cbd1496d9d227c035e3af
Lusine-Karapetyan <karapetyanlus.1991@gmail.com>
2023-03-05 19:59:49 +0400

    Created Person

---
+++ BooksProject/BooksProject/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksProject
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (age < 2 && age > 99)
+                {
+                    age = value;
+                }
+            }
+
+
+        }
+
+    }
+}
