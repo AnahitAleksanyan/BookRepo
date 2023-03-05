16c6451d5d9ea4c5f195061891094fe659536d33
Lusine-Karapetyan <karapetyanlus.1991@gmail.com>
2023-03-05 21:19:03 +0400

    Change Author propert type in Book

--- BooksProject/BooksProject/Book.cs
+++ BooksProject/BooksProject/Book.cs
@@ -4,7 +4,7 @@
     {
         public string Name { get; set; }
 
-        public string Author { get; set; }
+        public Person Author { get; set; }
 
         public int PageCount { get; set; }
 
